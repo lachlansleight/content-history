1adfacaa910c602df11b6fa6f67567701a6333f9
Lachlan Sleight <lachlansleight@gmail.com>
2023-05-29 21:46:05 +1000

    feat: Add pretty icons to tiles

--- components/SpotifyEvent.tsx
+++ components/SpotifyEvent.tsx
@@ -1,13 +1,15 @@
+import { FaSpotify } from "react-icons/fa";
 import { SpotifyContentEvent } from "lib/types";
 
 const SpotifyEvent = ({ event }: { event: SpotifyContentEvent }): JSX.Element => {
     return (
-        <div className="flex flex-col bg-green-200 bg-opacity-10 rounded-lg p-2">
+        <div className="flex flex-col bg-green-200 bg-opacity-10 rounded-lg p-2 relative">
             <h2 className="text-lg font-bold">{event.title}</h2>
             <h3 className="italic mb-2">
                 {event.artistName || ""} - {event.albumName || ""}
             </h3>
             <p>{event.time.toLocaleString()}</p>
+            <FaSpotify className="absolute bottom-2 right-2 text-green-400 text-2xl" />
         </div>
     );
 };
--- components/YoutubeEvent.tsx
+++ components/YoutubeEvent.tsx
@@ -1,11 +1,13 @@
+import { FaYoutube } from "react-icons/fa";
 import { YoutubeContentEvent } from "lib/types";
 
 const YoutubeEvent = ({ event }: { event: YoutubeContentEvent }): JSX.Element => {
     return (
-        <div className="flex flex-col bg-red-200 bg-opacity-10 rounded-lg p-2">
+        <div className="flex flex-col bg-red-200 bg-opacity-10 rounded-lg p-2 relative">
             <h2 className="text-lg font-bold">{event.title}</h2>
             <h3 className="italic mb-2">{event.channelName || ""}</h3>
             <p>{event.time.toLocaleString()}</p>
+            <FaYoutube className="absolute bottom-2 right-2 text-red-400 text-2xl" />
         </div>
     );
 };
